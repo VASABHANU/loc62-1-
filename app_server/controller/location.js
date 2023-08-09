755f0fac2078059849a51bd33d9262feb5fba3a9
VASABHANU <21eg107a62@anurag.edu.in>
2023-08-09 10:37:02 +0530

    Add the view templates

---
+++ app_server/controller/location.js
@@ -0,0 +1,18 @@
+module.exports.homelist=function(req,res){
+    res.render('location-list',{title:'Home'});
+};
+module.exports.locationinfo=function(req,res){
+    res.render('location-info',{title:'Location Info'});
+};
+module.exports.locationinfo2=function(req,res){
+    res.render('location-info2',{title:'Location Info2'});
+};
+module.exports.locationinfo3=function(req,res){
+    res.render('location-info3',{title:'Location Info3'});
+};
+module.exports.addReview=function(req,res){
+    res.render('location-Review-Form',{title:'Add Review'});
+};
+
+
+
